45d60b5a8f69546bbd41c7d580e830f004e5b674
mohammad0612 <mai212228@gmail.com>
2024-02-18 07:21:15 +0000

    implemented webhook make.com button

--- frontend/app/test/page.jsx
+++ frontend/app/test/page.jsx
@@ -43,11 +43,32 @@ function App() {
     }
   };
 
+  const handleButtonClick4 = async () => {
+    try {
+      const response = await fetch('/api/webhook', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: 'Hello World!' }),
+      });
+  
+      if (response.ok) {
+        console.log('Success');
+      } else {
+        console.error('Error:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div>
       <button className="btn" onClick={handleButtonClick}>Call create users post /api/write</button>
       <button className="btn" onClick={handleButtonClick2}>Call get all data /api/retrieve</button>
       <button className="btn" onClick={handleButtonClick3}>Call delete post /api/delete</button>
+      <button className="btn btn-active btn-secondary" onClick={handleButtonClick4}>Call webhook post /api/webhook</button>
     </div>
   );
 }
